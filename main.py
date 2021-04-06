601513dc5df99b2b3131968d5551ca41829c8b21
oatandjam <lieblain@outlook.com>
2021-04-06 19:09:43 +0200

    fixed recursive downloads and more

--- main.py
+++ main.py
@@ -3,44 +3,43 @@ try:
     from rich.text import Text
     from rich.prompt import Prompt
     from rich.table import Table
+    from rich import box
     import feedparser as fp
     import os
     import subprocess
 except ImportError:
-    print("Please make sure that all missing dependancies are installed!")
+    print("Please make sure that all requiered dependancies are installed!")
     quit() 
 #DOWNLOAD PATH
-location = ("/downloads/nyaatorrents/")
-nyaa_rss = fp.parse('https://nyaa.si/?page=rss')
-print(nyaa_rss['feed']['link'])
-entries = nyaa_rss['entries']
-
-console = Console()
-
+path = ("/downloads/nyaatorrents/")
+try:
+    nyaa_rss = fp.parse('https://nyaa.si/?page=rss')
+except:
+    print("Could not establish connection to nyaa.si.\nPlease make sure you are connected to the internet and try again.")
+    exit()
+else:
+    print(nyaa_rss['feed']['link'])
+    entries = nyaa_rss['entries']
+    
 def torrent_table():
-    table = Table(title="Nyaa.si Torrents")
+    console = Console()
+    table = Table(title = "Nyaa.si Torrents", box = box.ROUNDED)
     table.add_column(style="green")
     table.add_column("Name")
-    table.add_column("Link", style="blue")
+    table.add_column("Link", style = "blue")
     for entry in entries:
         table.add_row(str(entries.index(entry)),entry['title'],entry['link'])
     console.print(table)
 torrent_table()
 
 torrent_dl = Prompt.ask("Enter the the number of the torrent(s) you would like to download seperated by a comma")
-torrent_dl = torrent_dl.split(",")
-def torrent_download(*args):
-    i=0
-    for item in args:
-        print("Downloading " +str(args))
-        link = (entries[int(item[i])]['link'])
-        print(i)
-        i = i + 1
-        cmd = ("wget " +link +" -P " +location)
-        subprocess.call((cmd), shell=True)
-torrent_download(torrent_dl)
-
-
+torrent_dl = tuple(torrent_dl.split(","))
+print(torrent_dl)
+def torrent_download():
+        link = (entries[int(item)]['link'])
+        cmd = ("wget " +link +" -P " +path)
+        subprocess.call((cmd), shell = True)
+for item in torrent_dl:
+    torrent_download()
 print("Downloads complete!")
-
-exit=input("Press any key to exit...")
+exit()
