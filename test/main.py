82a9a5a99a3e00ff05c2055c5f5ee1c6fa126520
oatandjam <lieblain@outlook.com>
2021-04-06 19:12:38 +0200

    Delete test directory

--- test/main.py
+++
@@ -1,8 +0,0 @@
-import os, subprocess, sys
-
-def dl_choice(*args):
-    for arg in args:
-        rent = (nyaa_rss['entries'][arg]['link'])
-        cmd = ("wget " +rent +" -P " +location)
-        subprocess.call((cmd), shell=True)
-dl_choice(5)
